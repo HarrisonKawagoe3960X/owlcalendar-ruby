9ed68659499a9d2bc52ad29abd5759f19e0af327
HarrisonKawagoe3960X <hyzitbios@hotmail.com>
2019-11-25 02:05:40 +0900

    asdasdasdasd

--- app/controllers/newschedule_controller.rb
+++ app/controllers/newschedule_controller.rb
@@ -10,23 +10,27 @@ class NewscheduleController < ApplicationController
   minute = params[:minute]
   content = params[:content]
   temp = year.to_s
-  timenum = year.to_i * 1000+ month.to_i * 100  + day.to_i * 10 +hour.to_i  +minute.to_i * 0.1
+  timenum = year.to_i * 1000
   if month.to_i <10
     temp+="0"
   end
   temp += month.to_s
+  timenum += ( month.to_i * 100)
   if day.to_i <10
     temp+="0"
   end
   temp += day.to_s
+  timenum += (day.to_i * 10)
   if hour.to_i <10
     temp+="0"
   end
   temp += hour.to_s
+  timenum += hour.to_i
   if minute.to_i <10
     temp+="0"
   end
   temp += minute.to_s
+  timenum += (minute.to_i * 0.1)
   schedule = Schedule.new(content:content,time:timenum,userid:session[:userid],timedata:temp)
   schedule.save
   Schedule.order(:time)
